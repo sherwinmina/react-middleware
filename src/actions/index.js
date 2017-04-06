b9106dd0dc018c191d865827151121b14acec0e9
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-06 11:36:26 -0700

    added axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,13 @@
+import axios from 'axios';
 import {
   FETCH_USERS
 } from './types';
 
 export function fetchUsers() {
+  const request = axios.get('https://jsonplaceholder.typicode.com/users');
+  
   return {
     type: FETCH_USERS,
-    payload: [
-      {name: 'Jane'},
-      {name: 'Sherwin'},
-      {name: 'Jessica'},
-      {name: 'Arriane'}
-    ]
-  }
+    payload: request
+  };
 }
