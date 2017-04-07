ecb6e151901ffa908f639a8b4a6c382efad5cd57
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-07 11:51:04 -0700

    handling async

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,13 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+// If action does not have payload
+// or, the payload does not have a .then property
+// we dont't care about it, sent it on
 
-    next(action);
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+
+    console.log(''We don't have a promise', action);
   }
 }
